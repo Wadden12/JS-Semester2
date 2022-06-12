c05f138aa3eff4668a720088b598bbe553354719
Mike Wadden <95223545+Wadden12@users.noreply.github.com>
2022-06-12 09:10:20 -0230

    Add files via upload

---
+++ Semester 2/Week 5/script.js
@@ -0,0 +1,39 @@
+var pi= 3.14159;
+var radius = 4.2;
+var area;
+area = pi * radius *radius
+console.log("Area = " + area + "/n");
+var a =10;
+a +=  10;
+console.log(a);
+
+a++;
+console.log(a);
+// is && is and
+// || is or
+// ! not
+// ? 
+// : not
+// === strict equal
+// !== strict not equal
+
+ //var value = 5;
+ //for(var i=1;i<=10;i++){
+    
+ //}
+//console.log(`${value} *  ${i} = ${value * i}`);
+var even;
+var sum;
+for(var i = 1; i>=100; i++){
+    even = i % 2;
+    i+=i;
+    console.log(even);
+    if (even =0){
+        console.log(`Even Number is ${i} the sum is ${sum +=i}`);} 
+        else{
+            console.log("Test");
+        }
+        }
+   
+
+
